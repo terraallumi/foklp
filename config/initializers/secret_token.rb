d74ca1eed639d608f7b7a067a26e21911201c51d
Terra Allumi <terraallumi@gmail.com>
2013-02-20 00:12:34 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foklp::Application.config.secret_token = 'f532a7bae5e0c65f7e12f5a06a3d02cb32d993c4bd2f6058945b12d80f3ecaea4ef6958521a54ba3142d0b6d91e102dabba2a97fa598172b156af0170383df07'
